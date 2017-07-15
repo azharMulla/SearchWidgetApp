3a8d518b7fe68d94ac520062dd9c81fed740af10
Azhar Mulla <mulla.azzi@gmail.com>
2017-07-15 19:16:01 +0530

    First Commit
    Initiated Grunt and Other dependencies.
    Success @ first execution

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            example: {
+                port: 9000
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-connect');
+    grunt.registerTask('default', 'connect:example');
+
+};
