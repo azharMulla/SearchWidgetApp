e3f0f7ad5adafa470f6a23c48622a218aaaf1960
Azhar Mulla <mulla.azzi@gmail.com>
2017-07-15 20:32:11 +0530

    Added filter for searching Name and Policy
    Added a special filter to search the given input in Name and Policy
    Number

--- app/app.js
+++ app/app.js
@@ -7,4 +7,13 @@
 
     var module = angular.module("SearchWidgetApp",[]);
 
+    // Filter, which will search for entered text in Name and Policy Number
+    module.filter('nameAndPolicyFilter', function(){
+        return function(list,searchItem){
+            return list.filter(function(item){
+                return (item.name.toLowerCase().indexOf(searchItem) > -1 || item.policyNum.toLowerCase().indexOf(searchItem) > -1)
+            });
+        }
+    });
+
 }());
